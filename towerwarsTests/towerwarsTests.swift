c26e3bddcaf5524346b3d05ba9745b4538b7b2dd
abegehr <anton.begehr@icloud.com>
2018-09-28 10:15:59 +0200

    Initial Commit

---
+++ towerwarsTests/towerwarsTests.swift
@@ -0,0 +1,34 @@
+//
+//  towerwarsTests.swift
+//  towerwarsTests
+//
+//  Created by Anton Begehr on 28.09.18.
+//  Copyright © 2018 Anton Begehr. All rights reserved.
+//
+
+import XCTest
+@testable import towerwars
+
+class towerwarsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
