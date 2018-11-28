15ff23313419afe49ffcdb286d9c06479fe4673a
abegehr <anton.begehr@icloud.com>
2018-11-28 22:11:56 +0100

    enable user interaction

--- towerwars/TouchableShapeNode.swift
+++ towerwars/TouchableShapeNode.swift
@@ -10,6 +10,16 @@ import SpriteKit
 
 class TouchableShapeNode: SKShapeNode {
     
+    override init() {
+        super.init()
+        
+        self.isUserInteractionEnabled = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         print("touched")
     }
