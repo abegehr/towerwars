477be81c1d0f33ca583d538297c18b616066bb6d
abegehr <anton.begehr@icloud.com>
2018-12-13 20:41:58 +0100

    remove unnecessary functions

--- towerwars/TWHybridCreep.swift
+++ towerwars/TWHybridCreep.swift
@@ -11,15 +11,6 @@ import GameplayKit
 
 class TWHybridCreep: TWCreep {
     
-    init(position: CGPoint, team: Team, entityManager: TWEntityManager) {
-        
-        super.init(position:position, team: team, entityManager: entityManager)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     override func kill() {
         // remove entity
         entityManager.remove(self)
