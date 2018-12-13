42b17076fccde1f450c388fcf99d0897a16f8c6a
abegehr <anton.begehr@icloud.com>
2018-12-13 20:00:28 +0100

    add TWMiniCreep

---
+++ towerwars/TWMiniCreep.swift
@@ -0,0 +1,28 @@
+//
+//  TWMiniCreep.swift
+//  towerwars
+//
+//  Created by Anton Begehr on 13.12.18.
+//  Copyright © 2018 Anton Begehr. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class TWMiniCreep: TWCreep {
+    
+    init(position: CGPoint, team: Team, entityManager: TWEntityManager) {
+        
+        super.init(radius: 8, fillColor: TWPink, position:position, team: team, entityManager: entityManager)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func kill() {
+        // remove entity
+        entityManager.remove(self)
+    }
+    
+}
